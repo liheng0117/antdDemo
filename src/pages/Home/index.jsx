1d316977cb5cf7432424f440900caf36a306d427
liheng <tangcuyu@163.com>
2020-05-16 21:51:28 +0800

    Layout布局二

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Layout, Menu, Icon, Input, Avatar } from "antd";
+import { getUser } from "@/actions/auth";
+import { Layout, Menu, Icon, Input, Avatar, message, Progress } from "antd";
 import "./style.less";
 import headImg from "@/utils/img/header.png";
 
 const { Search } = Input;
-const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
+const MyIcon = Icon.createFromIconfontCN({
+  scriptUrl: "//at.alicdn.com/t/font_1776886_p7fq40292oi.js", // 在 iconfont.cn 上生成
+});
+
 export default
-@connect((state) => {
-  return {
-    user: state.auth.user,
-  };
-})
+@connect(
+  (state) => {
+    return {
+      user: state.auth.user,
+    };
+  },
+  {
+    getUser,
+  }
+)
 class Home extends Component {
+  //  路由守卫
+  constructor(props) {
+    super(props);
+    if (!this.props.user) {
+      message.info("请先登录");
+      this.props.history.push("/login");
+    }
+  }
+  //  退出登录
+  export = () => {
+    this.props.getUser("");
+    this.props.history.push("/login");
+  };
   render() {
     return (
       <div className="home">
@@ -38,60 +60,67 @@ class Home extends Component {
                   icon="user"
                 />
                 <b>{this.props.user}</b>
-                <Icon type="export" />
+                <Icon type="export" onClick={this.export} />
               </p>
             </div>
           </Header>
           <Layout>
-            <Sider width={200} style={{ background: "#fff" }}>
+            <Sider className="home-sider">
               <Menu
                 mode="inline"
                 defaultSelectedKeys={["1"]}
-                defaultOpenKeys={["sub1"]}
                 style={{ height: "100%", borderRight: 0 }}
               >
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <Icon type="user" />
-                      subnav 1
-                    </span>
-                  }
-                >
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={
-                    <span>
-                      <Icon type="laptop" />
-                      subnav 2
-                    </span>
-                  }
-                >
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  title={
-                    <span>
-                      <Icon type="notification" />
-                      subnav 3
-                    </span>
-                  }
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
+                <Menu.Item key="1">
+                  <span>Dashboard</span>
+                  <MyIcon type="iconshouye" />
+                </Menu.Item>
+                <Menu.Item key="2">
+                  <span>Basic UI Element</span>
+                  <MyIcon type="iconfenxiang" />
+                </Menu.Item>
+                <Menu.Item key="3">
+                  <span>Icons</span>
+                  <MyIcon type="iconcafe" />
+                </Menu.Item>
+                <Menu.Item key="4">
+                  <span>Form Elements</span>
+                  <MyIcon type="iconyoujian" />
+                </Menu.Item>
+                <Menu.Item key="5">
+                  <span>Chart</span>
+                  <MyIcon type="iconbicycle" />
+                </Menu.Item>
+                <Menu.Item key="6">
+                  <span>Table</span>
+                  <MyIcon type="iconios" />
+                </Menu.Item>
+                <Menu.Item key="7">
+                  <span>Sample Pages</span>
+                  <MyIcon type="iconhotel" />
+                </Menu.Item>
+                <div className="sider-bottom">
+                  <p>Total Sales</p>
+                  <Progress
+                    strokeColor={{
+                      "0%": "#87cc8b",
+                      "100%": "#eed457",
+                    }}
+                    percent={70}
+                    showInfo={false}
+                  />
+                  <span>50 Items sold</span>
+                  <p>Customer Target</p>
+                  <Progress
+                    strokeColor={{
+                      "0%": "#5130f4",
+                      "100%": "#a450df",
+                    }}
+                    percent={90}
+                    showInfo={false}
+                  />
+                  <span>200 Items sold</span>
+                </div>
               </Menu>
             </Sider>
             <Layout style={{ padding: "20px" }}>
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,3 @@
-import home from "@/reducer/home";
 import auth from "@/reducer/auth";
 
-export { home, auth };
+export { auth };
