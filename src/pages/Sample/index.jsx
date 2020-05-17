5833bcd414bd0c79ff41a23df744439c7c1faed7
liheng <tangcuyu@163.com>
2020-05-17 12:56:19 +0800

    pages

--- src/pages/Sample/index.jsx
+++ src/pages/Sample/index.jsx
@@ -1,7 +1,49 @@
 import React, { Component } from "react";
+import { Pagination } from "antd";
+import { post } from "@/utils/request";
+import api from "@/services/api";
+import "./style.less";
 
 export default class Sample extends Component {
+  state = {
+    data: [],
+    count: 1,
+  };
+  pageFn = (current) => {
+    let obj = { page: current };
+    post(api.pageUrl, obj).then((res) => {
+      this.setState({
+        data: res.result.list,
+        count: current,
+      });
+    });
+  };
+  componentDidMount() {
+    let obj = { page: this.state.count };
+    post(api.pageUrl, obj).then((res) => {
+      console.log(res);
+      this.setState({
+        data: res.result.list,
+      });
+    });
+  }
   render() {
-    return <div>Sample</div>;
+    const { data } = this.state;
+    return (
+      <div className="sample">
+        {data.map((v) => {
+          return (
+            <div key={v.id} className="content">
+              <p>{v.title}</p>
+            </div>
+          );
+        })}
+        <Pagination
+          defaultCurrent={this.state.count}
+          total={500}
+          onChange={this.pageFn}
+        />
+      </div>
+    );
   }
 }
--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,5 @@ export default {
   regUrl: "http://api.baxiaobu.com/index.php/home/v1/register",
   listUrl: "https://api.baxiaobu.com/index.php/home/v5/getuser",
   addUrl: "https://api.baxiaobu.com/index.php/home/v5/add",
+  pageUrl: "https://blogs.zdldove.top/Home/Apis/listWithPage",
 };
